7bd371a13a23ded036954f46f2236660873894e1
Camila Mier Cadena <camimiercadena@gmail.com>
2023-08-04 21:17:54 -0500

    mapa -sgl

---
+++ geolocalizacion_android/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Maps;
+
+namespace geolocalizacion_android
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetCurrentLocation();
+        }
+
+        private async void GetCurrentLocation()
+        {
+            try
+            {
+                var location = await Geolocation.GetLocationAsync();
+                if (location != null)
+                {
+                    map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromKilometers(1)));
+                }
+            }
+            catch (Exception ex)
+            {
+                // Maneja cualquier error al obtener la ubicación
+            }
+        }
+    }
+}
+
